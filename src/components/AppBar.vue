aee6feb1091e842887d00993a667299c3f94e033
Etienne FLIECX <etienne.fliecx@kleegroup.com>
2021-04-21 16:02:49 +0200

    Améliorations AppBar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,8 +1,15 @@
 <template>
     <v-app-bar app color="#27292E" flat>
-        <v-container class="fill-height">
-            <v-avatar class="mr-10" color="white" size="32"></v-avatar>
-        </v-container>
+        <v-avatar class="mr-10" color="white" size="32"></v-avatar>
+        <v-divider class="mx-4" vertical dark></v-divider>
+        <span class="subheading white--text">Page</span>
+        <v-spacer></v-spacer>
+        <v-divider class="mx-4" vertical dark></v-divider>
+        <div class="mr-16 white--text">Profil</div>
+        <v-divider class="mx-4" vertical dark></v-divider>
+        <v-app-bar-nav-icon color="#9292E3"></v-app-bar-nav-icon>
+        <v-divider class="mx-4" vertical dark></v-divider>
+        <v-avatar color="white" size="32"></v-avatar>
     </v-app-bar>
 </template>
 
