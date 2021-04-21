2f58cb3523d323239e0d7726176de0d6c140560f
Etienne FLIECX <etienne.fliecx@kleegroup.com>
2021-04-21 18:40:54 +0200

    WIP : Création d'un composant card

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-card class="mx-auto" max-width="344" outlined>
+        <v-list-item three-line>
+            <v-list-item-content>
+                <v-list-item-title class="headline mb-1">
+                    Produit
+                    <v-icon large class="icon">mdi-credit-card</v-icon>
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                    <div>Nombre</div>
+                    <div>1000</div>
+                </v-list-item-subtitle>
+            </v-list-item-content>
+        </v-list-item>
+    </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+<style lang="scss">
+.v-icon {
+    color: $secondary !important;
+    float: right;
+}
+.v-list-item__subtitle {
+    color: $secondary !important;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,19 @@
 <template>
-    <div>Hello</div>
+    <v-container>
+        <v-row no-gutters>
+            <v-col cols="3"><ProductCard /></v-col>
+            <v-col cols="3"><ProductCard /></v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-@Component({})
+import ProductCard from '@/components/ProductCard.vue'
+@Component({
+    components: {
+        ProductCard
+    }
+})
 export default class Home extends Vue {}
 </script>
